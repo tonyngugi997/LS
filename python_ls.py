ba3f4897a4ea5c8008e8fe35091838cfaa2c088c
tonyngugi997 <tonyngugi997@gmail.com>
2026-03-14 12:30:12 +0300

    feat: added simple file listing

--- python_ls.py
+++ python_ls.py
@@ -9,11 +9,27 @@ import argparse
 import sys
 from pathlib import Path
 
-class pyLS:
-    def __init__(self):
-            '''Initialize the pyLS class'''
+
+class PyLS:
+    def __init__(self, location="."):
+        """Initialize the PyLS class"""
+        self.location = location
+
+    def list_files(self):
+        """List files in the specified location"""
+        try:
+            files = os.listdir(self.location)
+
+            for file in files:
+                print(file)
+
+        except FileNotFoundError:
+            print(f"ERROR: The directory {self.location} does not exist.")
+
+        except PermissionError:
+            print(f"ERROR: Permission denied for: {self.location}.")
 
 
 if __name__ == "__main__":
-      my_ls = pyLS()
-      
+    my_ls = PyLS()
+    my_ls.list_files()
