b799ede496c2789c06f16c83bb15ea23d84e1409
tonyngugi997 <tonyngugi997@gmail.com>
2026-03-14 12:16:23 +0300

    basic class stracture

---
+++ python_ls.py
@@ -0,0 +1,19 @@
+"""
+pyls - Python implementation of Unix ls command
+Author: tonyngugi997
+GitHub: https://github.com/tonyngugi997/LS
+"""
+
+import os
+import argparse
+import sys
+from pathlib import Path
+
+class pyLS:
+    def __init__(self):
+            '''Initialize the pyLS class'''
+
+
+if __name__ == "__main__":
+      my_ls = pyLS()
+      
